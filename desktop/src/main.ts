28c8f34edfe09fef043885b2193d3869d5b8367a
Marvellous Nwachukwu <marvellousn561@gmail.com>
2026-06-20 14:53:02 +0100

    feat(app): Chain panel

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -1,5 +1,6 @@
 import { mountConsole } from "./console";
 import { buildPanel } from "./tools/build";
 import { followPanel } from "./tools/follow";
+import { chainPanel } from "./tools/chain";
 
-mountConsole([buildPanel, followPanel]);
+mountConsole([buildPanel, followPanel, chainPanel]);
