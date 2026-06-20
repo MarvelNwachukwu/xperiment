8aad89967243435f34b521b57b16629f041494a4
Marvellous Nwachukwu <marvellousn561@gmail.com>
2026-06-20 14:55:22 +0100

    feat(app): Unfollow panel (scan/review/unfollow)

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -2,5 +2,6 @@ import { mountConsole } from "./console";
 import { buildPanel } from "./tools/build";
 import { followPanel } from "./tools/follow";
 import { chainPanel } from "./tools/chain";
+import { unfollowPanel } from "./tools/unfollow";
 
-mountConsole([buildPanel, followPanel, chainPanel]);
+mountConsole([buildPanel, followPanel, chainPanel, unfollowPanel]);
